830aaf62a7d7e2483ab2ae58f94a690d1188f48a
Vincent Daubry <vdaubry@gmail.com>
2015-06-21 15:53:11 +0200

    add twitter app dev credentials

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,12 @@
 var oauth_callback = 'http://localhost:666/callback/';
-var oauth_consumer_key = 'NwA6HpLGxsDgFhUaWpvvIV8aQ';
-var oauth_consumer_secret = 'BbsF9Bqb2sUz1h7KHyTTuIGSPLlAjuZDQ8otzvoYXQSjlbo22r';
+
+//Twitter app Prod:
+//var oauth_consumer_key = 'NwA6HpLGxsDgFhUaWpvvIV8aQ';
+//var oauth_consumer_secret = 'BbsF9Bqb2sUz1h7KHyTTuIGSPLlAjuZDQ8otzvoYXQSjlbo22r';
+
+//Twitter app dev
+var oauth_consumer_key = 'acWguFLGgV5VUiNFRE6rrnaaM';
+var oauth_consumer_secret = 'Yr1QgIobfMpVIEkO4vKk3wmdgZIoRXplSlfKNVJsC9dYHD9vUB';
 
 var linkastor_url = 'http://localhost:5000';
 
